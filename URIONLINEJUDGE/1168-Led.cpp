e670aca68a47950aaf5425aafa4b02ee739de8c6
Martín Javier Ayala Miranda <martin.ayala0297@gmail.com>
2016-07-16 15:06:44 -0500

    Create 1168-Led.cpp

---
+++ URIONLINEJUDGE/1168-Led.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+map <char,int> led;
+int main(){
+    int n;
+    string s;
+    led['0'] = led['6'] = led['9'] = 6;
+    led['1'] = 2;
+    led['2'] = led['3'] =  led['5'] =  5;
+    led['4'] = 4;
+    led['7'] = 3;
+    led['8'] = 7;
+    scanf("%d",&n);
+    while(n--){
+        int acum = 0;
+        cin >> s;
+        for(int i = 0; i<(int)s.size(); i++){
+            acum+=led[s[i]];
+        }
+        printf("%d\n leds",acum);
+    }
+
+return 0;
+}
