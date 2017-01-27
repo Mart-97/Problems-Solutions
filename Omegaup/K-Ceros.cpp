73695e79a2a8827e6dca91df69f8ad6c6f5c4ea8
Martín Javier Ayala Miranda <martin.ayala0297@gmail.com>
2017-01-27 18:16:50 -0500

    Update K-Ceros.cpp

--- Omegaup/K-Ceros.cpp
+++ Omegaup/K-Ceros.cpp
@@ -1,70 +1,46 @@
-#include <cstdio>
-#include <map>
+#include <bits/stdc++.h>
 #define MAXN 80000000015
-using namespace std;
-long long  int m,medio;
-long long int contador = 0;
-  long long  int cantidad;
-/*int fiveSerie()
-{
-   for(long long int  i = 5; i <= MAXN; i+=5)
-   {
-       contador++;
-   }
-    printf("%lld",contador );
-    return 0;
-}
-*/
-long long int  zeros(long long int d){
 
+using namespace std;
+typedef long long int lli;
+
+lli zeros(lli n){
 long long int res;
 long long int c = 0;
- while(d!=0){
-      res=d/5;
+ while(n!=0){
+      res=n/5;
       c+=res;
-      d=res;
+      n=res;
 
     }
     return c;
 }
-map <int,int> mapa;
-
-int main()
-{
-    scanf("%lld",&m);
-    //fiveSerie();
-    long long int inicio = 0;
-   long long int fin = MAXN;
-
-  while(inicio < fin)
-  {
-        medio = (inicio + fin) / 2;
-  cantidad = zeros(medio);
-
- if(cantidad == m)
-           {
-              medio = medio - medio % 5;
-
-           printf("%lld",medio);
-               return 0;
-              }
-      if( cantidad < m)
-      {
-          if(cantidad + 1 == m) {
-                medio = medio - medio % 5;
-            printf("%lld",medio + 5);
-            return 0;
-          }
-          inicio = medio - 1;
-
-      } else
-      {
-          if( cantidad > m)
-          {
-              fin = medio + 1;
-          }
-
-      }
+int main(){
+  lli lo = 1,hi,mid,m;
+  hi = MAXN;
+  cin >> m;
+  ///printf("%lld\n",hi);
+  bool found = false;
+  while(lo < hi ){
+    mid = (lo + hi) / 2;
+    if(zeros(mid)==m){
+        printf("%lld\n",mid - (mid % 5));
+        found = true;
+        break;
+    }else {
+        if(zeros(mid) <  m){
+            if(zeros(mid)+ 1 == m){
+                printf("%lld\n",mid - (mid % 5) + 5);
+                found = true;
+                break;
+            }
+            lo = mid + 1 ;
+        }else {
+            hi = mid - 1;
+        }
+    }
   }
-    return 0;
+  if(!found) printf("%lld",lo);
+
+return 0;
 }
